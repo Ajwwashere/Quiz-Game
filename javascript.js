5ecf416261e4c68c3c003e3582b1c44df6e933bb
Ajwwashere <ajwalls0224@gmail.com>
2021-01-13 20:39:55 -0500

    small changes

--- javascript.js
+++ javascript.js
@@ -1,2 +1,10 @@
-console.log("it works right?")
+// console.log("it works right?")
+
+var h2Tag = document.createElement("h2");
+document.body.appendChild(h2Tag);
+h2Tag.textContent = "Click the start button to start the test, and the timer. Once the timer runs out, or you answer all questions, the test will be over. Incorrect answers will remove time from the timer so chose your answers carefully. Good Luck!";
+h2Tag.style.textAlign = "center";
+h2Tag.style.textDecoration = "fade"
+
+var addButton = document.querySelector("#start");
 
